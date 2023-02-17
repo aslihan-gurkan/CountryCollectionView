1858496a714172e6fae201317667ff5b46ae1e2d
aslihan-gurkan <aslihangurkan07@gmail.com>
2023-02-17 14:46:18 +0300

    CollectionViewCell
    learn how to use CollectionViewCell

---
+++ CountryCollectionView/CountryCell.swift
@@ -0,0 +1,14 @@
+//
+//  CountryCell.swift
+//  CountryCollectionView
+//
+//  Created by Aslıhan Gürkan on 17.02.2023.
+//
+
+import UIKit
+
+class CountryCell: UICollectionViewCell {
+    
+    @IBOutlet weak var countryImageView: UIImageView!
+    
+}
--- CountryCollectionView/ViewController.swift
+++ CountryCollectionView/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var countryCollectionView: UICollectionView!
+    
+    let countries = ["turkey", "canada", "brazil", "spain", "japan", "uk", "usa"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        countryCollectionView.delegate = self
+        countryCollectionView.dataSource = self
     }
-
-
 }
 
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return countries.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = countryCollectionView.dequeueReusableCell(withReuseIdentifier: "countryCell", for: indexPath) as! CountryCell
+        
+        cell.countryImageView.image = UIImage(named: countries[indexPath.row])
+//        cell.layer.cornerRadius = cell.frame.height / 2
+        
+        return cell
+    }
+    
+    
+    
+}
